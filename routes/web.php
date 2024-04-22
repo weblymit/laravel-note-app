9a09e28557b5aed36e32e3bd3be03fdba868f5da
DevMit <weblymit@gmail.com>
2024-04-22 19:09:08 +0200

    create pages + routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+// Route::get('/note', [NoteController::class, 'index'])->name('notes.index');
+// Route::get('/note/create', [NoteController::class, 'create'])->name('notes.create');
+// Route::post('/note', [NoteController::class, 'store'])->name('notes.store');
+// Route::get('/note/{id}', [NoteController::class, 'show'])->name('notes.show');
+// Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('notes.edit');
+// Route::put('/note/{id}', [NoteController::class, 'update'])->name('notes.update');
+// Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('notes.destroy');
+
+
+// Route::resource('notes', NoteController::class)->only([
+//   'index', 'create', 'store', 'show', 'edit', 'update', 'destroy'
+// ]);
+
+Route::resource('note', NoteController::class);
